6d69a95b5bfd02de28416380309b04b8ef498600
Isaac Leonardo M <mcisaacleonardo@gmail.com>
2024-02-04 18:27:36 -0400

    Beginning styles

--- resources/views/cards/index.blade.php
+++ resources/views/cards/index.blade.php
@@ -4,6 +4,39 @@
         <meta charset="utf-8">
         <meta name="viewport" content="width=device-width, initial-scale=1">
         <title>Cards List Pagination</title>
+
+        <style>
+            ul.transactions-list {
+                list-style: none;
+                padding: 0;
+                max-width: 500px;
+                margin: 0 auto;
+            }
+
+            li.transaction {
+                display: flex;
+                justify-content: space-between;
+                align-items: center;
+                padding: 10px;
+                margin-bottom: 10px;
+                border: 1px solid;
+                border-radius: 25px;
+            }
+
+            .transaction-icon > * {
+                width: 70px;
+                height: 70px;
+                border-radius: 50%;
+            }
+
+            .recharge-icon {
+                background-color: rgb(60 134 94);
+            }
+            
+            .travel-icon {
+                background-color: rgb(167 75 75);
+            }
+        </style>
     </head>
     <body>
         <h1>Historial de Transacciones</h1>
@@ -18,9 +51,13 @@
                 <li class="transaction">
                     <div class="transaction-icon">
                         @if ($card->recharge)
-                            <p>recarga</p>
+                            <div class="recharge-icon">
+
+                            </div>
                         @else
-                            <p>viaje</p>
+                            <div class="travel-icon">
+
+                            </div>
                         @endif
                     </div>
 
